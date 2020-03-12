35b45b5da75bc9dbf30f32ada8409b52666f0b0f
Christian <seidchr@gmail.com>
2020-03-12 11:42:41 +0000

    remove obsolete usings

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,7 @@
 #nullable enable
 namespace Corona
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Globalization;
-    using System.IO;
-    using System.Linq;
-    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
-    using Corona.CsvModel;
-    using Corona.GithubClient;
-    using Corona.PlotModel;
-    using FileHelpers;
-    using ScottPlot;
 
     public class Program
     {
